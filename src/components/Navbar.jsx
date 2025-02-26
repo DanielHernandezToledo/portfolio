9274787a3faca28aec9d1470acfb02e25c11a1af
Daniel Hernandez Toledo <daniel.aht1993@gmail.com>
2025-02-26 00:04:33 -0300

    correccion url linkedin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 </a>
             </div>
             <div className="m8 flex items-center justify-center gap-4 text-2xl">
-                <a href="https://www.linkedin.com/in/daniel-hernandez-2b8b6b1b8/" target="_blank" rel="noopener noreferrer" aria-label='Linkedin'><FaLinkedin /></a>
+                <a href="https://www.linkedin.com/in/danielhernandezdev/" target="_blank" rel="noopener noreferrer" aria-label='Linkedin'><FaLinkedin /></a>
                 <a href="https://github.com/DanielHernandezToledo" target="_blank" rel="noopener noreferrer" aria-label='Github'><FaGithub /></a>
                 <a href="https://www.instagram.com/daniel.aht93/" target="_blank" rel="noopener noreferrer" aria-label='Instagrm'><FaInstagram /></a>
             </div>
